df3f4954123f9e012db0bb7f747e18b75d2356f2
Sirdoogles <55865274+Sirdoogles@users.noreply.github.com>
2019-10-11 22:18:39 -0700

    Update Makefile.sh

--- Makefile.sh
+++ Makefile.sh
@@ -9,7 +9,7 @@ classB.o: classB.cpp
 	g++ -Wall -c classB.cpp -o classB.o
 
 base.o: base.cpp
-	g++	-Wall -c base.cpp -o base.o
+	g++ -Wall -c base.cpp -o base.o
 
 test.o: test.cpp
 	g++ -Wall -c test.cpp -o test.o
