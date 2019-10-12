84361b24242ffaa55c17a537fa1cacacbbdf810c
Sirdoogles <55865274+Sirdoogles@users.noreply.github.com>
2019-10-11 19:35:00 -0700

    Add files via upload

--- Makefile.sh
+++ Makefile.sh
@@ -2,17 +2,17 @@
 # print out each command being executed 
 all: test.x
 
-test.x: classA.o classB.o test.o 
-	g++ classA.o classB.o test.o -o test.x
+classA.o: classA.cpp
+	g++ -Wall -c classA.cpp -o test.o
+
+classB.o: classB.cpp
+	g++ -Wall -c classB.cpp -o test.o
 
 test.o: test.cpp
 	g++ -Wall -c test.cpp -o test.o
 
-classA.o: classA.cpp classA.h base.h
-	g++ -Wall -c classA.cpp -o test.o
-
-classB.o: classB.cpp classB.h base.h
-	g++ -Wall -c classB.cpp -o test.o
+test.x: test.o classA.o classB.o 
+	g++ test.o classA.o classB.o -o test.x
 
 clean:
 	rm -rf *o all
